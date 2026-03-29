19e73d253733a690b4329a5b18ce60872d607a47
radoslaw.panuszewski <radoslaw.panuszewski@allegro.com>
2026-03-29 16:41:22 +0200

    Prepare for integration with gh-bot-orchestrator

--- allwrite-cli/src/main/kotlin/pl/allegro/tech/allwrite/cli/infrastructure/bot/GithubBotModule.kt
+++ allwrite-cli/src/main/kotlin/pl/allegro/tech/allwrite/cli/infrastructure/bot/GithubBotModule.kt
@@ -1,8 +1,8 @@
-package pl.allegro.tech.allwrite.cli.infrastructure.pullrequestmanager
+package pl.allegro.tech.allwrite.cli.infrastructure.bot
 
 import org.koin.core.annotation.ComponentScan
 import org.koin.core.annotation.Module
 
 @Module
 @ComponentScan
-public class PullRequestManagerModule
+public class GithubBotModule
