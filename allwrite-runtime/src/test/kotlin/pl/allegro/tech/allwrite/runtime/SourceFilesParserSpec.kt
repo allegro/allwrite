b1164678c20cc88d10218b9a86c45f83c6104db4
PawelBalcerek <pawel.balcerek@allegro.com>
2026-05-08 08:21:58 +0200

    [#44] test (deeply) nested ClasspathAwareRecipe behavior

--- allwrite-runtime/src/test/kotlin/pl/allegro/tech/allwrite/runtime/SourceFilesParserSpec.kt
+++ allwrite-runtime/src/test/kotlin/pl/allegro/tech/allwrite/runtime/SourceFilesParserSpec.kt
@@ -1,9 +1,16 @@
 package pl.allegro.tech.allwrite.runtime
 
 import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
+import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeInstanceOf
 import org.koin.core.component.inject
 import org.openrewrite.InMemoryExecutionContext
+import org.openrewrite.java.tree.J
+import org.openrewrite.java.tree.JavaType
 import pl.allegro.tech.allwrite.runtime.base.BaseRuntimeSpec
+import pl.allegro.tech.allwrite.runtime.fake.FakeClasspathAwareRecipe
+import pl.allegro.tech.allwrite.runtime.fake.FakeCompositeRecipe
 import pl.allegro.tech.allwrite.runtime.fake.FakeParsingAwareRecipe
 import pl.allegro.tech.allwrite.runtime.fake.FakeRecipe
 import java.nio.file.Paths
@@ -26,6 +33,7 @@ class SourceFilesParserSpec : BaseRuntimeSpec() {
             parsedFiles.map { it.sourcePath } shouldContainExactlyInAnyOrder listOf(
                 Paths.get("src/testFixtures/inputFilesForTests/interesting-dir/some-file.yaml"),
                 Paths.get("src/testFixtures/inputFilesForTests/interesting-dir/another-file.yaml"),
+                Paths.get("src/testFixtures/inputFilesForTests/java-dir/MyConfig.java"),
                 Paths.get("src/testFixtures/inputFilesForTests/large-file.yaml"),
             )
         }
@@ -43,6 +51,60 @@ class SourceFilesParserSpec : BaseRuntimeSpec() {
                 Paths.get("src/testFixtures/inputFilesForTests/interesting-dir/some-file.yaml"),
                 Paths.get("src/testFixtures/inputFilesForTests/interesting-dir/another-file.yaml"),
             )
+        }
+
+        test("should resolve classpath from a direct ClasspathAwareRecipe") {
+            // given
+            val recipe = FakeClasspathAwareRecipe(classpath = listOf("spring-context-6"))
+            val inputFiles = listOf(Paths.get("src/testFixtures/inputFilesForTests/java-dir/MyConfig.java"))
+
+            // when
+            val parsedFiles = sourceFilesParser.parseSourceFiles(recipe, inputFiles, InMemoryExecutionContext())
+
+            // then
+            val compilationUnit = parsedFiles.single().shouldBeInstanceOf<J.CompilationUnit>()
+            val annotationType = compilationUnit.classes.single().leadingAnnotations.single().annotationType
+            val resolvedType = (annotationType as J.Identifier).type
+            resolvedType.shouldNotBeNull()
+            val classType = resolvedType.shouldBeInstanceOf<JavaType.Class>()
+            classType.fullyQualifiedName shouldBe "org.springframework.context.annotation.Configuration"
+        }
+
+        test("should resolve classpath from ClasspathAwareRecipe nested in a composite recipe") {
+            // given
+            val classpathAwareSubRecipe = FakeClasspathAwareRecipe(classpath = listOf("spring-context-6"))
+            val compositeRecipe = FakeCompositeRecipe(classpathAwareSubRecipe)
+            val inputFiles = listOf(Paths.get("src/testFixtures/inputFilesForTests/java-dir/MyConfig.java"))
+
+            // when
+            val parsedFiles = sourceFilesParser.parseSourceFiles(compositeRecipe, inputFiles, InMemoryExecutionContext())
+
+            // then
+            val compilationUnit = parsedFiles.single().shouldBeInstanceOf<J.CompilationUnit>()
+            val annotationType = compilationUnit.classes.single().leadingAnnotations.single().annotationType
+            val resolvedType = (annotationType as J.Identifier).type
+            resolvedType.shouldNotBeNull()
+            val classType = resolvedType.shouldBeInstanceOf<JavaType.Class>()
+            classType.fullyQualifiedName shouldBe "org.springframework.context.annotation.Configuration"
+        }
+
+        test("should resolve classpath from ClasspathAwareRecipe deeply nested in composite recipes") {
+            // given
+            val classpathAwareSubRecipe = FakeClasspathAwareRecipe(classpath = listOf("spring-context-6"))
+            val innerComposite = FakeCompositeRecipe(FakeRecipe(), classpathAwareSubRecipe)
+            val outerComposite = FakeCompositeRecipe(innerComposite)
+            val inputFiles = listOf(Paths.get("src/testFixtures/inputFilesForTests/java-dir/MyConfig.java"))
+
+            // when
+            val parsedFiles = sourceFilesParser.parseSourceFiles(outerComposite, inputFiles, InMemoryExecutionContext())
+
+            // then
+            val compilationUnit = parsedFiles.single().shouldBeInstanceOf<J.CompilationUnit>()
+            val annotationType = compilationUnit.classes.single().leadingAnnotations.single().annotationType
+            val resolvedType = (annotationType as J.Identifier).type
+            resolvedType.shouldNotBeNull()
+            val classType = resolvedType.shouldBeInstanceOf<JavaType.Class>()
+            classType.fullyQualifiedName shouldBe "org.springframework.context.annotation.Configuration"
         }
     }
 }
---
+++ allwrite-runtime/src/testFixtures/inputFilesForTests/java-dir/MyConfig.java
@@ -0,0 +1,5 @@
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class MyConfig {
+}
