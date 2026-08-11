c7fc730f295a876e60cf1c2307ede3cc1cee338e
Kamil Patora <kamil.patora@allegro.com>
2026-08-11 11:24:21 +0200

    Revert "test: add test for ignoring recipes without CLI coordinates in RecipeMatcher"
    This reverts commit 9a92b87af14442560b63bf8a2ff3e66d33d208c5.

--- allwrite-cli/src/test/kotlin/pl/allegro/tech/allwrite/cli/RecipeMatcherSpec.kt
+++ allwrite-cli/src/test/kotlin/pl/allegro/tech/allwrite/cli/RecipeMatcherSpec.kt
@@ -2,7 +2,6 @@ package pl.allegro.tech.allwrite.cli
 
 import com.github.zafarkhaja.semver.Version
 import io.kotest.matchers.equals.shouldBeEqual
-import org.koin.core.module.Module
 import org.koin.ksp.generated.module
 import pl.allegro.tech.allwrite.api.RecipeCoordinates
 import pl.allegro.tech.allwrite.cli.application.RecipeMatcher
@@ -14,7 +13,7 @@ import pl.allegro.tech.allwrite.runtime.util.injectEagerly
 
 class RecipeMatcherSpec : BaseCliSpec() {
 
-    override fun additionalModules(): List<Module> =
+    override fun additionalModules() =
         listOf(
             FakeRuntimeModule().module,
         )
@@ -41,32 +40,6 @@ class RecipeMatcherSpec : BaseCliSpec() {
             // then
             recipeDescriptors shouldBeEqual
                 listOf(FakeRecipeSource.Companion.SPRING_BOOT_3_TEST_RECIPE.descriptor, FakeRecipeSource.Companion.SPRING_BOOT_4_TEST_RECIPE.descriptor)
-        }
-
-        test("should ignore recipes without CLI coordinates") {
-            // given
-            val nonCliRecipe = FakeRecipe(
-                id = "pl.allegro.tech.allwrite.recipes.internal-helper",
-                tags = setOf("visibility:PUBLIC"),
-            )
-            val matcher = RecipeMatcher(
-                FakeRecipeSource(
-                    FakeRecipeSource.Companion.SPRING_BOOT_3_TEST_RECIPE,
-                    FakeRecipeSource.Companion.SPRING_BOOT_4_TEST_RECIPE,
-                    nonCliRecipe,
-                ),
-            )
-
-            // when
-            val recipeDescriptors = matcher.findMatching(
-                RecipeCoordinates("spring-boot", "upgrade", null, null),
-            )
-
-            // then
-            recipeDescriptors shouldBeEqual listOf(
-                FakeRecipeSource.Companion.SPRING_BOOT_3_TEST_RECIPE.descriptor,
-                FakeRecipeSource.Companion.SPRING_BOOT_4_TEST_RECIPE.descriptor,
-            )
         }
 
         test("should return empty list when no exact match and versions are specified") {
