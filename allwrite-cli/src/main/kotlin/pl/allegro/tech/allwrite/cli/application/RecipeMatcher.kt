5a01a69eaffda42b8b39b4e4373cf2fd294a298c
Kamil Patora <kamil.patora@allegro.com>
2026-08-11 11:24:25 +0200

    Revert "fix: filter out null recipe coordinates in RecipeMatcher"
    This reverts commit 0f21bfc176ce817a55158e6e8f79d12048664308.

--- allwrite-cli/src/main/kotlin/pl/allegro/tech/allwrite/cli/application/RecipeMatcher.kt
+++ allwrite-cli/src/main/kotlin/pl/allegro/tech/allwrite/cli/application/RecipeMatcher.kt
@@ -8,7 +8,6 @@ import pl.allegro.tech.allwrite.api.RecipeSource
 import pl.allegro.tech.allwrite.api.getFromVersion
 import pl.allegro.tech.allwrite.api.getToVersion
 import pl.allegro.tech.allwrite.api.tagPropertyOrNull
-import pl.allegro.tech.allwrite.api.toRecipeCoordinatesOrNull
 
 @Single
 internal class RecipeMatcher(
@@ -18,7 +17,7 @@ internal class RecipeMatcher(
     fun findMatching(coordinates: RecipeCoordinates): List<RecipeDescriptor> {
         val (group, recipe, from, to) = coordinates
 
-        val descriptors = recipeSource.findAll().filter { it.toRecipeCoordinatesOrNull() != null }
+        val descriptors = recipeSource.findAll()
         val groupRecipes = descriptors
             .filter { group == it.tagPropertyOrNull("group") }
             .filter { recipe == it.tagPropertyOrNull("action") }
@@ -28,13 +27,15 @@ internal class RecipeMatcher(
     }
 
     fun findMatchingByTargetVersion(group: String, recipe: String, to: Version): List<RecipeDescriptor> {
-        val descriptors = recipeSource.findAll().filter { it.toRecipeCoordinatesOrNull() != null }
+        val descriptors = recipeSource.findAll()
         val groupRecipes = descriptors
             .filter { group == it.tagPropertyOrNull("group") }
             .filter { recipe == it.tagPropertyOrNull("action") }
             .filter { it.getFromVersion() != null && it.getToVersion() != null }
 
-        val lowestFrom = groupRecipes.minOfOrNull { it.getFromVersion()!! } ?: return emptyList()
+        val lowestFrom = groupRecipes
+            .map { it.getFromVersion()!! }
+            .minOrNull() ?: return emptyList()
 
         val directRecipe = groupRecipes
             .filter { lowestFrom.isSameMinorVersionAs(it.getFromVersion()) }
